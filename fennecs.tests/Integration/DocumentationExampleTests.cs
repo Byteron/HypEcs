4ffc3b6e8044b0cc73658752f67107f4cdfbabeb
Tiger Jove <tiger@jupiter.blue>
2024-02-09 16:34:59 +0100

    Working on fennecs godot sample

--- fennecs.tests/Integration/DocumentationExampleTests.cs
+++ fennecs.tests/Integration/DocumentationExampleTests.cs
@@ -24,4 +24,23 @@ public class DocumentationExampleTests
         expected = new Position(1, 2, 3) * MULTIPLIER;
         Assert.Equal(expected, pos2);
     }
+
+    [Theory]
+    [InlineData(20_000, 2_000)]
+    [InlineData(20_000, 1_999)]
+    [InlineData(20_000, 37)]
+    public void Can_Iterate_Multiple_Chunks(int count, int chunkSize)
+    {
+        using var world = new World();
+        for (int i = 0; i < 20_000; i++)
+        {
+            world.Spawn().Add(new Position(1,2,3)).Id();
+        }
+
+        var query = world.Query<Position>().Build();
+
+        query.RunParallel((ref Position pos) => { pos = new Position(2,3,4); }, chunkSize: chunkSize);
+
+        Assert.Equal(count, query.Count);
+    }
 }
